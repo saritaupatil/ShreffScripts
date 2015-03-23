e5149c903f8c45b1cf24bb3dceedc3658bcc4c69
cewashburn <cewashburn@mgh.harvard.edu>
2015-03-23 13:32:56 -0400

    Now identifies too-high concentrations

--- PhadiaReader.R
+++ PhadiaReader.R
@@ -1,5 +1,6 @@
 #script to pull data from the phadia100's .csv run files.
 
+rm(list=ls())
 #Set working directory
 setwd("C:/Users/cew27/Dropbox (Personal)/R") #you can add in your own director here
 
@@ -8,7 +9,7 @@ data <- read.csv("IgE-20150115.csv", header=T, skip = 7, na.strings = "")
 #seperate out the data we need and clean it up
 #Note that if you're looking at the file in excel, the concentration appears under the column "(quot)", but when you read it into r, it's under "X.Conc.". I can't figure out why. Seems to be the case for other columns as well. Double-check that you're getting the data you want.
 phadia <- data.frame(data$X.Request., data$X.Test., data$X.Conc., data$X.InsDil.)
-colnames(phadia) <- c("sampleID", "test", "concentration", "dilution")
+colnames(phadia) <- c("sampleID", "test", "conc", "dil")
 
 #Removes the junk leftover from the data summary at the bottom of the .csv and QC/CC data
 phadia <- phadia[complete.cases(phadia),]
@@ -24,7 +25,31 @@ phadia$subjectID <- gsub("_.*", "", phadia$subjectID)
 phadia$visitNum <- phadia$sampleID
 phadia$visitNum <- gsub(".*_", "", phadia$visitNum)
 
-#The raw data doesn't indicate if a concentration is off the scale, so we need to figure out which are extrapolated (>100)
-#My plan is to write some code that will check the if the concentration is > 100 * (dilution factor) for specific and 5000 * (dilution factor) for total IgE
-# if it's greater, it should replace the concentration with >100 or >5000
+#convert the concentration readings to numerics rather than factors.
 
+phadia$conc <- as.numeric(as.character(phadia$conc))
+
+#The raw data doesn't indicate if a concentration is off the scale, so we need to figure out which are extrapolated (>100 or < 0.1)
+
+#change under-the-limit concentrations to "0"
+v <- vector(mode="logical", length=0)
+for (i in 1:length(phadia$conc)){
+  v[i] <- (phadia$conc[i] < 0.1)
+}   
+phadia$conc[v] <- 0
+
+#change over-the-limit concentrations to ">"
+phadia$max <- phadia$dil*100
+
+for (i in 1:length(phadia$max)){
+  if (phadia$test[i] == "a-IgE") {phadia$max[i] = phadia$dil[i]*5000}
+}
+
+v <- vector(mode="logical", length=0)
+for (i in 1:length(phadia$conc)){
+  v[i] <- (phadia$conc[i] > phadia$max[i])
+}   
+phadia$conc[v] <- paste(">",phadia$max[v], sep="")
+
+#Then we need to split the data by project and read the data into the appropriate redcap.
+phad <- tapply(phadia, phadia$study)
